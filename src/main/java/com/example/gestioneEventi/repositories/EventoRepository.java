aef2ab7d9eaa4cd13d9bb1a5493040d082bc9339
vvillino <158823750+vvillino@users.noreply.github.com>
2024-12-17 16:28:14 +0100

    Update EventoRepository.java

--- src/main/java/com/example/gestioneEventi/repositories/EventoRepository.java
+++ src/main/java/com/example/gestioneEventi/repositories/EventoRepository.java
@@ -1,12 +1,9 @@
 package com.example.gestioneEventi.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.gestioneEventi.model.Evento;
-import com.example.gestioneEventi.model.Utente;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
