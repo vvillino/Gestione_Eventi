def50110672370605f90a4f3ae3d286e1f7a2aab
vvillino <158823750+vvillino@users.noreply.github.com>
2024-12-19 11:47:12 +0100

    Update UtenteRepository.java

--- src/main/java/com/example/gestioneEventi/repositories/UtenteRepository.java
+++ src/main/java/com/example/gestioneEventi/repositories/UtenteRepository.java
@@ -1,11 +1,19 @@
 package com.example.gestioneEventi.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.gestioneEventi.model.Utente;
 
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT U.nome, U.cognome FROM Utenti u JOIN Partecipanti p ON u.id_utente = p.id_Utente WHERE P.id_evento = :id_evento")
+    public List<Utente> findAllByEvento(@Param("id_evento") Long id);
 
 }
